b4e981d7b8b9a436ddc5f27ca05b25746d9d04fc
rubenhernandez-itch2 <81659471+rubenhernandez-itch2@users.noreply.github.com>
2025-01-30 17:40:41 -0600

    Add files via upload

---
+++ EVA1_2_STACKOVERFLOW/src/eva1_2_stackoverflow/EVA1_2_STACKOVERFLOW.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_2_stackoverflow;
+
+/**
+ *
+ * @author ruben
+ */
+public class EVA1_2_STACKOVERFLOW {
+    
+    public static void main(String[] args) {
+        A();
+    }
+    
+    public static void A(){
+        A();
+    }
+    
+}
